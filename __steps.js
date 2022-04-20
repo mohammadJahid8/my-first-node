1bcdbe50a7f726488f306f81c49a9b2a372ee6b8
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-04-20 19:36:47 +0600

    my first node

---
+++ __steps.js
@@ -0,0 +1,17 @@
+/* 
+1. creater a folder manually or by mkdir
+2. open command line to that folder 
+3. npm init 
+4.npm install express
+5.create index js
+
+----for express-------
+1.require express
+2.create app var
+3.declare port app
+4.set app.get('/)
+5.listen to port 
+6.node index.js
+7.check browser
+
+*/
